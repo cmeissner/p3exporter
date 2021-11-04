ce4238c252ed11dbe176f84210855fd239ac4a74
Christian Meißner <cme+github@meissner.sh>
2021-11-04 15:53:49 +0100

    make command_with_cache async

--- p3exporter/cache/async.py
+++ p3exporter/cache/async.py
@@ -2,18 +2,18 @@
 from datetime import datetime, timedelta
 from functools import lru_cache, wraps
 
-def async_command_with_cache(lifetime: int = 3600, maxsize: int = 128):
+async def async_command_with_cache(lifetime: int = 3600, maxsize: int = 128):
 
-    def wrapper_async(func):
+    async def wrapper_async(func):
         func = lru_cache(maxsize=maxsize)(func)
         func.lifetime = timedelta(seconds=lifetime)
         func.expiration = datetime.utcnow() + func.lifetime
 
         @wraps(func)
-        def wrapped_func(*args, **kwargs):
+        async def wrapped_func(*args, **kwargs):
 
             return func(*args, **kwargs)
 
-        return wrapped_func()
+        return await wrapped_func()
 
     return wrapper_async
